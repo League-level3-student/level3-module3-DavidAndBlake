7bb0c7ed86b9143d4dba1d16241e01dd8e0d6d8d
League Student <league@adminisorsiMac3.attlocal.net>
2018-11-18 17:25:37 -0800

    Started making a OPM adaptation of the farm recipe

---
+++ src/Monster.java
@@ -0,0 +1,6 @@
+
+public abstract class Monster {
+
+	abstract void makeNoise();
+	abstract void takeAShit();
+}
---
+++ src/MonsterRegistry.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class MonsterRegistry {
+
+	public static void main(String[] args) {
+		ArrayList<E>
+
+	}
+
+}
