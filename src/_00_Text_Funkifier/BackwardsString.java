dd4f3bd99cb5735e48e9fbdf0ab162a8dab47ac7
1luckybiscuit <lukasnepo11@gmail.com>
2018-11-29 16:56:30 -0800

    Almost finished

--- src/_00_Text_Funkifier/BackwardsString.java
+++ src/_00_Text_Funkifier/BackwardsString.java
@@ -12,7 +12,7 @@ public class BackwardsString extends SpecialString {
 		for (int i = s.length()-1; i >= 0; i--) {
 			backwardsMessage += s.charAt(i);
 		}
-		System.out.println(backwardsMessage);
+//		System.out.println(backwardsMessage);
 		return backwardsMessage;
 	}
 
---
+++ src/_00_Text_Funkifier/MyOwnString.java
@@ -0,0 +1,16 @@
+package _00_Text_Funkifier;
+
+public class MyOwnString extends SpecialString {
+
+	public MyOwnString(String s) {
+		super(s);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String funkifyText(String s) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
